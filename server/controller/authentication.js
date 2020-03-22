41eb53270c0e60f7fa80306be7f1756eb22623fe
Jeff Vincent Ansay <jeff_ansay@ligph.com>
2020-03-22 18:45:54 +0800

    Done App

---
+++ server/controller/authentication.js
@@ -0,0 +1,46 @@
+const jwt = require('jwt-simple');
+const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+  // User has already had their email and password auth'd
+  // We just need to give them a token
+  res.send({ token: tokenForUser(req.user) });
+}
+
+exports.signup = function(req, res, next) {
+  const { email, password } = req.body;
+
+  if(!email || !password) {
+    res.status(422).send({ error: 'You must provide email and password'});
+  }
+  // See if a user with the given email exists
+  User.findOne({ email }, (err, users) => {
+    if(err) { return next(err); }
+
+    // if a user with the does exist, return an error
+    if(users) {
+      return res.status(422).send({error: 'Email is in use'});
+    }
+
+    //if a user with email does not exist, create save user record
+    const user = new User({
+      email,
+      password
+    });
+
+    // Respond to request indicating the user was created
+    user.save((err) => {
+      if(err) { return next(err)}
+
+      // respond to request indicating the user was created
+      res.json({ token: tokenForUser(user) });
+    });
+  })
+
+}
---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+
+// DB Setup
+mongoose.connect('mongodb+srv://admin:admin123@devconnector-y5ctd.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
+
